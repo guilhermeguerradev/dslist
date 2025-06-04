06a9563a0e37881909a452d55b8a2c80a62284fb
guilhermeguerradev <guilhermeguerrajf@gmail.com>
2025-06-03 21:47:50 -0300

    Move games on list

--- dslist/src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ dslist/src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -2,13 +2,13 @@ package com.devsuperior.dslist.controllers;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMiniDTO;
+import com.devsuperior.dslist.dto.ReplacementDTO;
 import com.devsuperior.dslist.services.GameListService;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -33,4 +33,11 @@ public class GameListController {
         List<GameMiniDTO> result = gameService.findByList(listId);
         return result;
     }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+    }
+
+
 }
--- dslist/src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ dslist/src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -3,6 +3,8 @@ package com.devsuperior.dslist.repositories;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
     /*
@@ -16,5 +18,10 @@ public interface GameListRepository extends JpaRepository<GameList, Long> {
      * Essa camada isola a lógica de acesso ao banco de dados da camada de serviço (Service Layer),
      * promovendo melhor organização e manutenção do código.
      */
+
+    @Modifying
+    @Query(nativeQuery = true,
+            value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 
 }
